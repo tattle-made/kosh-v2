07d0c2bcd6917627e38110aba2bc75e535eae577
denny <denny.george90@gmail.com>
2021-02-01 23:40:04 +0530

    feat(ci): reinitialized repo

---
+++ src/backend/index.js
@@ -0,0 +1,16 @@
+// import express from "express";
+
+// const PORT = 8000;
+// const HOST = "0.0.0.0.";
+
+// const app = express();
+
+// app.get("/", (req, res) => {
+//   res.send("Hello World \n");
+// });
+
+// app.listen(PORT, HOST);
+
+// console.log("server running");
+
+console.log(process.env);
