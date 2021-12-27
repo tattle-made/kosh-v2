ea58e9c602ed89d87c409a22d821148679604504
Guru Prasad G <prasadguru141@gmail.com>
2021-12-27 12:04:54 +0530

    #14 | GP | added. APIs for index, blacklist

--- src/backend/app/data-source/repository-post.js
+++ src/backend/app/data-source/repository-post.js
@@ -1,6 +1,9 @@
+const { Op } = require("sequelize");
+const { Sequelize } = require("../../core/database/models");
 const db = require("../../core/database/models");
-const post = db.sequelize.models.post;
-const { getOne } = require("../../core/mongo");
+const {post, datasource, postIndexHistory: PostIndexHistory} = db.sequelize.models;
+const { getOne, get } = require("../../core/mongo");
+const axios= require('axios');
 
 const getPostById = async (datasourceId, postId) => {
   try {
@@ -51,4 +54,115 @@ const updatePostIndexStatus = async (indexHistory) => {
   }
 }
 
-module.exports = { getPostById, createPost, updatePostIndexStatus };
+const datasourceIndexStatus = async () => {
+  try {
+    const data = await post.findAll({
+        group: ['datasource', 'index_status'],
+        attributes: ['datasource', 'index_status', [Sequelize.fn('COUNT', 'index_status'), 'count']],
+        raw: true,
+        include: [{ model: datasource, as: 'dataset' }]
+    })
+    const responseStructure = {
+      datasourceName: "",
+      indexed: 0,
+      failed: 0,
+      blacklisted: 0
+    }
+    const result = {}
+    data.forEach((post) => {
+      if (!result[post.datasource]) result[post.datasource] = responseStructure
+      result[post.datasource].id = post.datasource
+      result[post.datasource].datasourceName = post["dataset.name"]
+      result[post.datasource][post.index_status] = post.count
+    })
+    return Object.values(result)
+  } catch (err) {
+    console.log("Error : Could not get index status");
+    throw err;
+  }
+}
+
+const postIndexStatus = async (datasourceId) => {
+  try {
+    const data = await post.findAll({
+        where: {
+          datasource: datasourceId
+        },
+    })
+    return data
+  } catch (err) {
+    console.log("Error : Could not get index status");
+    throw err;
+  }
+}
+
+const blacklistPostIndex = async (datasourceId) => {
+  try {
+    return await post.update({
+      index_status: 'blacklisted'
+    }, {
+      where: {
+        datasource: datasourceId,
+        index_status: { [Op.notIn]: ["enqueued", "indexed"]}
+      }
+    });
+  } catch (err) {
+    console.log("Error : Could not blacklist Post status");
+    throw err;
+  }
+}
+
+const indexPosts = async (accessToken, datasourceId) => {
+  const condition = [
+    { index_status: null },
+    { index_status: { [Op.notIn]: ["enqueued", "indexed"] } }
+  ]
+  if (datasourceId) condition.push({datasource: datasourceId})
+  let posts;
+  try {
+      posts = await post.findAll({
+          where: {
+              [Op.or]: condition,
+          },
+      });
+  } catch (e) {
+      console.log(e)
+      throw e;
+  }
+  const postsToIndex = {}
+  const indexHistory = []
+  posts.forEach((post) => {
+      postsToIndex[post.id] = { 
+          post: {
+              id: post.id,
+              media_type: post.type,
+              media_url: post.media_url
+          },
+          config: { mode: "store" }
+      }
+      indexHistory.push({
+          post_id: post.id,
+          status: "enqueued"
+      })
+  })
+  const METADATA_COLLECTION = "kosh_metadata"
+  const FACTCHECK_DB = "kosh_metadata"
+  const postsMetadata = await get(
+    FACTCHECK_DB,
+    METADATA_COLLECTION,
+    { e_kosh_id: { $in: Object.keys(postsToIndex) } },
+    { limit: 0, skip: 0 }
+  )
+  for await (const postMetadata of postsMetadata) {
+      if (!postMetadata.e_kosh_id || !Object.keys(postsToIndex).includes(postMetadata.e_kosh_id)) continue
+      postsToIndex[postMetadata.e_kosh_id]["metadata"] = {...postMetadata}
+  }
+  await PostIndexHistory.bulkCreate(indexHistory)
+  await axios.post(process.env.INDEX_API_URL + "/index", Object.values(postsToIndex), {
+      headers: { Authorization: "Bearer " + accessToken },
+  })
+}
+
+module.exports = { getPostById, createPost, updatePostIndexStatus,
+   datasourceIndexStatus, blacklistPostIndex, indexPosts, postIndexStatus };
+
--- src/backend/app/index-post/routes.js
+++ src/backend/app/index-post/routes.js
@@ -1,16 +1,17 @@
 const db = require("../../core/database/models");
 const { StatusCodes } = require("http-status-codes");
-const { updatePostIndexStatus } = require("../data-source/repository-post");
+const jwt = require("jsonwebtoken");
+const { updatePostIndexStatus, datasourceIndexStatus, postIndexStatus, blacklistPostIndex, indexPosts } = require("../data-source/repository-post");
 
 const PostIndexHistory = db.sequelize.models.postIndexHistory;
 
 const configure = (expressApp) => {
-    expressApp.post("/index/report", async (request, response) => {
+    expressApp.post("/index/report", async (req, res) => {
         const indexHistory = {
-            indexer_id: request.body.indexer_id,
-            post_id: request.body.post_id,
-            status_code: request.body.status_code,
-            status: request.body.status
+            indexer_id: req.body.indexer_id,
+            post_id: req.body.post_id,
+            status_code: req.body.status_code,
+            status: req.body.status
         }
         try {
             await PostIndexHistory.create(indexHistory)
@@ -19,10 +20,72 @@ const configure = (expressApp) => {
             console.log("Error : Could not create Index History");
             throw err;
         }
-        response
-        .status(StatusCodes.OK)
-        .send({ });
-    })
+        res
+            .status(StatusCodes.OK)
+            .send({});
+    });
+
+    expressApp.post("/index", async (req, res) => {
+        try {
+            const accessToken = jwt.sign(req.user, process.env.ACCESS_TOKEN_SECRET)
+            const post = await indexPosts(accessToken);
+            res.status(StatusCodes.OK).send(post);
+        } catch (err) {
+            console.log(err);
+            res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({ error: "Could not index" });
+        }
+    });
+
+    expressApp.get("/index/datasource", async (_req, res) => {
+        try {
+            const post = await datasourceIndexStatus()
+            res.status(StatusCodes.OK).send(post);
+        } catch (err) {
+            console.log(err);
+            res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({ error: "Could not get post index" });
+        }
+    });
+
+    expressApp.post("/index/datasource/:datasourceId", async (req, res) => {
+        try {
+            const accessToken = jwt.sign(req.user, process.env.ACCESS_TOKEN_SECRET)
+            const post = await indexPosts(accessToken, req.params.datasourceId)
+            res.status(StatusCodes.OK).send(post);
+        } catch (err) {
+            console.log(err);
+            res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({ error: "Could not index" });
+        }
+    });
+
+    expressApp.patch("/index/datasource/:datasourceId/blacklist", async (req, res) => {
+        try {
+            const post = await blacklistPostIndex(req.params.datasourceId)
+            res.status(StatusCodes.OK).send(post);
+        } catch (err) {
+            console.log(err);
+            res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({ error: "Could not blacklist index" });
+        }
+    });
+
+    expressApp.get("/index/datasource/:datasourceId/post", async (req, res) => {
+        try {
+            const post = await postIndexStatus(req.params.datasourceId)
+            res.status(StatusCodes.OK).send(post);
+        } catch (err) {
+            console.log(err);
+            res
+                .status(StatusCodes.INTERNAL_SERVER_ERROR)
+                .send({ error: "Could not get post index" });
+        }
+    });
 }
 
 module.exports = configure
